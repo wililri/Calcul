4529cca9d0f75f9d7965289543cf7c73d913f9da
Дилюк <@DESKTOP-5SJPVS1>
2024-03-09 12:12:06 +0300

    Добавьте файлы проекта.

---
+++ Calcul/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calcul
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Button_Click(object sender,RoutedEventArgs e)
+        {
+            InitializeComponent();
+            string operation=((Button)sender).Content.ToString();
+            double oper1 = Convert.ToDouble(Output_panel.Text);
+            double oper2 = Convert.ToDouble(panel.Text);
+            double res = 0;
+            switch (operation)
+            {
+                case "+":
+                    res=oper1+ oper2;
+                    break;
+                case "-":
+                    res=oper2 - oper1;
+                    break;
+                    case "*":  
+                    res=oper1 * oper2;
+                    break;
+                    case "/":
+                    res=oper1 / oper2;
+                    break;
+            }
+            panela.Text=res.ToString();
+        }
+
+        private void Output_panel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
